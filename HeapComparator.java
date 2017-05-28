37765ae07577442e3545a0f1a447a3291f3d1510
ElzbthGR <gabitova_03@mail.ru>
2017-05-28 22:06:52 +0300

    Heap

---
+++ HeapComparator.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Elizabeth on 25.05.2017.
+ */
+import java.util.Comparator;
+
+public class HeapComparator implements Comparator<Integer> {
+    public int compare(Integer a , Integer b){
+        return a.compareTo(b);
+    }
+}
+
